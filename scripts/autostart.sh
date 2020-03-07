46c433534be06eca4391c86427fac694c39c7dc4
cycleke <cycleke@gmail.com>
2020-03-08 00:12:15 +0800

    change some autostart

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -3,16 +3,16 @@
 # xrandr --auto --output DP-2 --same-as eDP-1 --size 1920x1080
 # xrandr --auto --output HDMI-1  --right-of DP-2 --size 1920x1080
 /bin/bash ~/scripts/dwm-status.sh &
-# /bin/bash ~/scripts/wp-autochange.sh &
+/bin/bash ~/scripts/wp-autochange.sh &
 # picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+picom -b
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
 nm-applet &
 xfce4-power-manager &
-xfce4-volumed-pulse &
-clipit &
+# xfce4-volumed-pulse &
 fcitx &
 redshift &
 nutstore &
-variety &
+fetchmail -k &
 ~/scripts/autostart_wait.sh &
