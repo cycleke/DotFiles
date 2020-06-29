20c8cb4ab87379d15b00b85e0a419640f36be9c0
cycleke <cycleke@gmail.com>
2020-06-29 22:39:07 +0800

    add fcitx5 and some other softwares' config

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -3,15 +3,13 @@
 xrandr --output eDP1 --primary --mode 1920x1080 --pos 1200x716 --rotate normal --output HDMI1 --mode 1920x1200 --pos 0x0 --rotate left --output DP1 --off --output HDMI2 --off
 /bin/bash ~/scripts/dwm-status.sh &5
 /bin/bash ~/scripts/wp-autochange.sh &
-# picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 picom -b
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
-nm-applet &
 xfce4-power-manager &
 # xfce4-volumed-pulse &
-fcitx &
 redshift &
-clash &
 nohup /usr/bin/nutstore >/dev/null 2>&1 &
+nm-applet &
+fcitx5 &
 ~/scripts/autostart_wait.sh &
--- zshenv
+++ zshenv
@@ -1,9 +1,9 @@
 export PATH=~/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/share/dotnet:$HOME/.dotnet/tools:$HOME/bin:$HOME/.local/bin:$HOME/.local/bin:$HOME/.npm-global/bin:$HOME/bin
 
 export EDITOR=nvim
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS="@im=fcitx"
+export GTK_IM_MODULE=fcitx5
+export QT_IM_MODULE=fcitx5
+export XMODIFIERS="@im=fcitx5"
 export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
 
 export LDFLAGS="-L/usr/local/opt/libxml2/lib"
